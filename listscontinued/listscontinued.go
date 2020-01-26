2b020bb3a0482aba6abfd3999bbdd308fe65581c
Elton SV <el10savio96@gmail.com>
2020-01-26 15:53:53 +0530

    encodeModified

--- listscontinued/listscontinued.go
+++ listscontinued/listscontinued.go
@@ -4,6 +4,7 @@ import (
 	"github.com/el10savio/Ninety-Nine-Golang-Problems/lists"
 )
 
+// Can't get it to omit struct value if not present
 func EncodeModified(list []string) ([]lists.RLE, error) {
 	packed, err := lists.Pack(list)
 	if err != nil {
@@ -13,7 +14,11 @@ func EncodeModified(list []string) ([]lists.RLE, error) {
 	encoded := make([]lists.RLE, 0)
 
 	for _, element := range packed {
-		encoded = append(encoded, lists.RLE{Count: len(element), Value: string(element[0])})
+		if len(element) > 1 {
+			encoded = append(encoded, lists.RLE{Count: len(element), Value: string(element[0])})
+		} else {
+			encoded = append(encoded, lists.RLE{Value: string(element[0])})
+		}
 	}
 
 	return encoded, nil
--- listscontinued/listscontinued_test.go
+++ listscontinued/listscontinued_test.go
@@ -1,1 +1,42 @@
 package listscontinued
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/el10savio/Ninety-Nine-Golang-Problems/lists"
+)
+
+func TestEncodeModified(t *testing.T) {
+	list := []string{"a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"}
+
+	expectedEncodeModified := []lists.RLE{{4, "a"}, {0, "b"}, {2, "c"}, {2, "a"}, {0, "d"}, {4, "e"}}
+	var expectedErr error
+
+	actualEncodeModified, actualErr := EncodeModified(list)
+
+	if !reflect.DeepEqual(expectedErr, actualErr) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedErr, actualErr)
+	}
+
+	if !reflect.DeepEqual(expectedEncodeModified, actualEncodeModified) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedEncodeModified, actualEncodeModified)
+	}
+}
+
+func TestEncodeModified_EmptyList(t *testing.T) {
+	list := []string{}
+
+	expectedEncodeModified := []lists.RLE{}
+	expectedErr := lists.ErrEmptyList
+
+	actualEncodeModified, actualErr := EncodeModified(list)
+
+	if !reflect.DeepEqual(expectedErr, actualErr) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedErr, actualErr)
+	}
+
+	if !reflect.DeepEqual(expectedEncodeModified, actualEncodeModified) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedEncodeModified, actualEncodeModified)
+	}
+}
