c014203664e919af81742e2fb5d4cb13c1fba66e
Elton SV <el10savio96@gmail.com>
2020-01-26 16:17:21 +0530

    decodeModified

--- listscontinued/listscontinued.go
+++ listscontinued/listscontinued.go
@@ -23,3 +23,20 @@ func EncodeModified(list []string) ([]lists.RLE, error) {
 
 	return encoded, nil
 }
+
+func DecodeModified(rleList []lists.RLE) ([]string, error) {
+	if len(rleList) < 1 {
+		return []string{}, lists.ErrEmptyList
+	}
+
+	decoded := make([]string, 0)
+
+	for _, element := range rleList {
+		decoded = append(decoded, element.Value)
+		for index := 1; index < element.Count; index++ {
+			decoded = append(decoded, element.Value)
+		}
+	}
+
+	return decoded, nil
+}
--- listscontinued/listscontinued_test.go
+++ listscontinued/listscontinued_test.go
@@ -40,3 +40,37 @@ func TestEncodeModified_EmptyList(t *testing.T) {
 		t.Fatalf("Expected: %v\n Got: %v\n", expectedEncodeModified, actualEncodeModified)
 	}
 }
+
+func TestDecodeModified(t *testing.T) {
+	list := []lists.RLE{{4, "a"}, {0, "b"}, {2, "c"}, {2, "a"}, {0, "d"}, {4, "e"}}
+
+	expectedDecodeModified := []string{"a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"}
+	var expectedErr error
+
+	actualDecodeModified, actualErr := DecodeModified(list)
+
+	if !reflect.DeepEqual(expectedErr, actualErr) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedErr, actualErr)
+	}
+
+	if !reflect.DeepEqual(expectedDecodeModified, actualDecodeModified) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedDecodeModified, actualDecodeModified)
+	}
+}
+
+func TestDecodeModified_EmptyList(t *testing.T) {
+	list := []lists.RLE{}
+
+	expectedDecodeModified := []string{}
+	expectedErr := lists.ErrEmptyList
+
+	actualDecodeModified, actualErr := DecodeModified(list)
+
+	if !reflect.DeepEqual(expectedErr, actualErr) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedErr, actualErr)
+	}
+
+	if !reflect.DeepEqual(expectedDecodeModified, actualDecodeModified) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedDecodeModified, actualDecodeModified)
+	}
+}
