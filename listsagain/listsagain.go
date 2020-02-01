e12bdaa52631ec183609c3bdca08cac309450b97
Elton SV <el10savio96@gmail.com>
2020-02-01 10:02:02 +0530

    insertAt

--- listsagain/listsagain.go
+++ listsagain/listsagain.go
@@ -1,2 +1,23 @@
 package listsagain
 
+import "github.com/el10savio/Ninety-Nine-Golang-Problems/definitions"
+
+func InsertAt(value int, list []int, insertPoint int) ([]int, error) {
+	length := len(list)
+
+	if length < 1 {
+		return []int{}, definitions.ErrEmptyList
+	}
+
+	insertPoint--
+
+	if insertPoint <= 0 || insertPoint > length {
+		return []int{}, definitions.ErrIndexOutOfRange
+	}
+
+	newList := append(list, 0)
+	copy(newList[insertPoint+1:], newList[insertPoint:])
+	newList[insertPoint] = value
+
+	return newList, nil
+}
--- listsagain/listsagain_test.go
+++ listsagain/listsagain_test.go
@@ -1,2 +1,103 @@
 package listsagain
 
+import (
+	"reflect"
+	"testing"
+
+	"github.com/el10savio/Ninety-Nine-Golang-Problems/definitions"
+)
+
+func TestInsertAt(t *testing.T) {
+	list := []int{1, 2, 3, 4}
+	insertPoint := 2
+	value := 7
+
+	expectedInsertAt := []int{1, 7, 2, 3, 4}
+	var expectedErr error
+
+	actualInsertAt, actualErr := InsertAt(value, list, insertPoint)
+
+	if !reflect.DeepEqual(expectedErr, actualErr) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedErr, actualErr)
+	}
+
+	if !reflect.DeepEqual(expectedInsertAt, actualInsertAt) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedInsertAt, actualInsertAt)
+	}
+}
+
+func TestInsertAt_EmptyList(t *testing.T) {
+	list := []int{}
+	insertPoint := 2
+	value := 7
+
+	expectedInsertAt := []int{}
+	expectedErr := definitions.ErrEmptyList
+
+	actualInsertAt, actualErr := InsertAt(value, list, insertPoint)
+
+	if !reflect.DeepEqual(expectedErr, actualErr) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedErr, actualErr)
+	}
+
+	if !reflect.DeepEqual(expectedInsertAt, actualInsertAt) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedInsertAt, actualInsertAt)
+	}
+}
+
+func TestInsertAt_NegativeInsertPoint(t *testing.T) {
+	list := []int{1, 2, 3, 4}
+	insertPoint := -2
+	value := 7
+
+	expectedInsertAt := []int{}
+	expectedErr := definitions.ErrIndexOutOfRange
+
+	actualInsertAt, actualErr := InsertAt(value, list, insertPoint)
+
+	if !reflect.DeepEqual(expectedErr, actualErr) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedErr, actualErr)
+	}
+
+	if !reflect.DeepEqual(expectedInsertAt, actualInsertAt) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedInsertAt, actualInsertAt)
+	}
+}
+
+func TestInsertAt_OutOfRangeInsertPoint(t *testing.T) {
+	list := []int{1, 2, 3, 4}
+	insertPoint := 12
+	value := 7
+
+	expectedInsertAt := []int{}
+	expectedErr := definitions.ErrIndexOutOfRange
+
+	actualInsertAt, actualErr := InsertAt(value, list, insertPoint)
+
+	if !reflect.DeepEqual(expectedErr, actualErr) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedErr, actualErr)
+	}
+
+	if !reflect.DeepEqual(expectedInsertAt, actualInsertAt) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedInsertAt, actualInsertAt)
+	}
+}
+
+func TestInsertAt_ZeroInsertPoint(t *testing.T) {
+	list := []int{1, 2, 3, 4}
+	insertPoint := 0
+	value := 7
+
+	expectedInsertAt := []int{}
+	expectedErr := definitions.ErrIndexOutOfRange
+
+	actualInsertAt, actualErr := InsertAt(value, list, insertPoint)
+
+	if !reflect.DeepEqual(expectedErr, actualErr) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedErr, actualErr)
+	}
+
+	if !reflect.DeepEqual(expectedInsertAt, actualInsertAt) {
+		t.Fatalf("Expected: %v\n Got: %v\n", expectedInsertAt, actualInsertAt)
+	}
+}
