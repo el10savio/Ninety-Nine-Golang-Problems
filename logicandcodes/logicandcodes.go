8fa0320dad775c076f3669e68a7040c5793f7fa0
Elton SV <el10savio96@gmail.com>
2020-03-29 16:49:25 +0530

    Init GrayCodes

--- logicandcodes/logicandcodes.go
+++ logicandcodes/logicandcodes.go
@@ -1,1 +1,29 @@
 package logicandcodes
+
+import (
+	"log"
+
+	"github.com/el10savio/Ninety-Nine-Golang-Problems/definitions"
+	"github.com/el10savio/Ninety-Nine-Golang-Problems/listsagain"
+)
+
+func GrayCodes(number int) ([]string, error) {
+	if number < 1 {
+		return []string{}, definitions.ErrNotPositiveNumber
+	}
+
+	baseValues := []string{"0", "1"}
+
+	if number == 1 {
+		return baseValues, nil
+	}
+
+	combinations, err := listsagain.Combinations(number, baseValues)
+	if err != nil {
+		return []string{}, err
+	}
+
+	log.Println(combinations)
+
+	return []string{}, nil
+}
--- logicandcodes/logicandcodes_test.go
+++ logicandcodes/logicandcodes_test.go
@@ -1,1 +1,33 @@
 package logicandcodes
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/el10savio/Ninety-Nine-Golang-Problems/definitions"
+)
+
+type testCase struct {
+	Number   int
+	GrayCode []string
+	Err      error
+}
+
+var testCases = []testCase{
+	testCase{-1, []string{}, definitions.ErrNotPositiveNumber},
+	testCase{0, []string{}, definitions.ErrNotPositiveNumber},
+	testCase{1, []string{"0", "1"}, nil},
+	testCase{2, []string{"00", "01", "11", "10"}, nil},
+	testCase{3, []string{"000", "001", "011", "010", "110", "111", "101", "100"}, nil},
+}
+
+func TestParser(t *testing.T) {
+	for _, testCase := range testCases {
+		t.Run("", func(t *testing.T) {
+			actualGrayCode, actualErr := GrayCodes(testCase.Number)
+			assert.Equal(t, testCase.Err, actualErr)
+			assert.Equal(t, testCase.GrayCode, actualGrayCode)
+		})
+	}
+}
